14ace6c0e9a539e13def3cc4da00a803b2b593f1
link857 <2761766788@qq.com>
2021-03-20 03:14:18 +0800

    build a radio-sending structure and environment

--- 智能云拐杖副板.py
+++ 智能云拐杖副板.py
@@ -1,9 +1,14 @@
 from mpython import *
-import math
 import radio
+
+#收到‘call’表示发送提醒与定位到app上，收到数字即为心率
 
 radio.on()
 radio.config(channel=13)
-sense_pulse = radio.receive()    #只接收心率
-#发送心率到app
-#倒地提醒与报警拨号直接由主板发到app上
+sense_pulse = 0
+while True:
+    if radio.receive() != 'call':
+        sense_pulse = radio.receive()
+        #发送心率到app
+    elif radio.receive() == 'call':
+        #发送提醒与定位到app上
