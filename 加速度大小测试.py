3ed7a8252425ac5f0d3c87a30e4c0fd9f2865841
link857 <2761766788@qq.com>
2021-03-19 21:25:43 +0800

    add test values

--- 加速度大小测试.py
+++ 加速度大小测试.py
@@ -1,6 +1,15 @@
 #mPythonType:0
 from mpython import *
 
+import time
+
+# 1.角度  
+# 2.测试加速度大小：
+#   (1)正常状态（走路或站立）：
+#      x轴加速度区间[0.5429,0.8710]
+#      y轴加速度区间[-0.8710,0.5703]
+#      z轴加速度区间[0.1320,0.6562]
+
 while True:
     oled.fill(0)
     x1 = accelerometer.get_x()
@@ -13,3 +22,7 @@ while True:
     oled.DispChar('加速度 z', 3, 40, 1)
     oled.DispChar((str(z1)), 52, 40, 1)
     oled.show()
+    
+#x1 >= 0.5429 and x1 <= 0.8710
+#y1 >= -0.8710 and y1 <= 0.5703
+#z1 >= 0.1320 and z1 <= 0.6562
