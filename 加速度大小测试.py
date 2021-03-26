e174dc0eefecbeb5377a478976e1263a8e84523e
link857 <2761766788@qq.com>
2021-03-26 20:21:39 +0800

    reset

---
+++ 加速度大小测试.py
@@ -0,0 +1,15 @@
+from mpython import *
+import time
+
+while True:
+    oled.fill(0)
+    x1 = accelerometer.get_x()
+    y1 = accelerometer.get_y()
+    z1 = accelerometer.get_z()
+    oled.DispChar('加速度 x', 3, 11, 1)
+    oled.DispChar((str(x1)), 52, 11, 1)
+    oled.DispChar('加速度 y', 3, 22, 1)
+    oled.DispChar((str(y1)), 52, 22, 1)
+    oled.DispChar('加速度 z', 3, 33, 1)
+    oled.DispChar((str(z1)), 52, 33, 1)
+    oled.show()
