69854756d41ae2b0d129e9078a871adafd391594
link857 <2761766788@qq.com>
2021-03-26 20:28:34 +0800

    test accelerator

--- 加速度大小测试.py
+++ 加速度大小测试.py
@@ -2,14 +2,13 @@ from mpython import *
 import time
 
 while True:
+    x = accelerometer.get_x()
+    y = accelerometer.get_y()
+    z = accelerometer.get_z()
     oled.fill(0)
-    x1 = accelerometer.get_x()
-    y1 = accelerometer.get_y()
-    z1 = accelerometer.get_z()
-    oled.DispChar('加速度 x', 3, 11, 1)
-    oled.DispChar((str(x1)), 52, 11, 1)
-    oled.DispChar('加速度 y', 3, 22, 1)
-    oled.DispChar((str(y1)), 52, 22, 1)
-    oled.DispChar('加速度 z', 3, 33, 1)
-    oled.DispChar((str(z1)), 52, 33, 1)
+    oled.DispChar('加速度大小测试：', 0, 0, 1)
+    oled.DispChar(('x轴：' + str(x)), 0, 16, 1)
+    oled.DispChar(('y轴：' + str(y)), 0, 32, 1)
+    oled.DispChar(('z轴：' + str(z)), 0, 48, 1)
     oled.show()
+    time.sleep(0.25)
---
+++ 加速度角度测试.py
@@ -0,0 +1,34 @@
+#mPythonType:0
+from mpython import *
+import time
+
+def get_tilt_angle(_axis):
+    x = accelerometer.get_x()
+    y = accelerometer.get_y()
+    z = accelerometer.get_z()
+    if 'X' == _axis:
+        force = math.sqrt(y ** 2 + z ** 2)
+        if z < 0: return math.degrees(math.atan2(x , force))
+        else: return 180 - math.degrees(math.atan2(x , force))
+    elif 'Y' == _axis:
+        force = math.sqrt(x ** 2 + z ** 2)
+        if _Az < 0: return  math.degrees(math.atan2(y , force))
+        else: return 180 - math.degrees(math.atan2(y , force))
+    elif 'Z' == _axis:
+        force = math.sqrt(x ** 2 + y ** 2)
+        if (x + y) < 0: return 180 - math.degrees(math.atan2(force , z))
+        else: return math.degrees(math.atan2(force , z)) - 180
+    return 0
+
+angle_x = get_tilt_angle('X')
+angle_y = get_tilt_angle('Y')
+angle_z = get_tilt_angle('Z')
+
+while True:
+    oled.fill(0)
+    oled.DispChar('加速度角度测试：', 0, 0, 1)
+    oled.DispChar(('x轴：' + str(angle_x)), 0, 16, 1)
+    oled.DispChar(('y轴：' + str(angle_y)), 0, 32, 1)
+    oled.DispChar(('z轴：' + str(angle_z)), 0, 48, 1)
+    oled.show()
+    time.sleep(0.2)
