cb021ae7da0286dca9abfd138b24b874bd6bc2c4
link857 <2761766788@qq.com>
2021-04-17 03:02:14 +0800

    整理代码

--- 交互示例.py
+++ 交互示例.py
@@ -2,18 +2,29 @@ import urequests
 
 # heartbeat
 
-def thread():
-
+def fall_det_thread():
+    global status, heartbeat_Loc, lat_Fall, lon_fall
     while True:
+        status = "ok"
+        heartbeat_Loc = None
+        if fall = 1:
+            #获取定位
+            lat_fall = ...
+            lon_fall = ...
+            loc_fall = {"latitude":lat_fall, 
+                            "longtitude":lon_fall}
+            status = "emergency"
+            heartbeat_Loc = loc_fall
 
+
+def heartbeat_thread():
+    while True:
         BASE_URL = '127.0.0.1:8000/demoboard'
 
         data = {
         "uuid": uuid,
         "status": status,
-        "loc": {
-            "latitude": 0,
-            "longitude": 0
+        "loc": heartbeat_Loc
         }
 
         resp = urequests.post(url=BASE_URL+'/heartbeat/', data=data)
--- 守护者云拐杖主板（多线程版）.py
+++ 守护者云拐杖主板（多线程版）.py
@@ -11,14 +11,12 @@ import sys
 import time
 import urequests
 
-
-
+#引脚
 #p16&p15：串口uart1（待测试）
 #p0&p1：小方舟模块
 #p13：灯带1
 #p14：灯带2
-#p5：“回家”按钮
-
+#p5：“带我回家”按钮
 
 #摔倒判断：角度
 
@@ -28,10 +26,11 @@ my_rgb1 = neopixel.NeoPixel(Pin(Pin.P13), n=21, bpp=3, timing=1)#引脚设定
 my_rgb2 = neopixel.NeoPixel(Pin(Pin.P15), n=21, bpp=3, timing=1)
 p5 = MPythonPin(5, PinMode.IN)
 
-
-
 BASE_URL = '/demoboard'
+
 uuid = '14159265358979313530481716qfpkydy666'
+status = ""
+heartbeat_Loc = {}
 
 
 #初始化服务器传输
@@ -39,21 +38,34 @@ my_wifi = wifi()         #搭建WiFi，连接app用户手机数据
 mywifi.connectWiFi("QFCS1","12345678")
 
 
-                                            
-backhome = 0
+#全局变量定义                                            
+backhome = 0    #1：按下带我回家按钮；   0：导航到家或空状态
 move = 0
 fall = 0
 time_on = 0
 switch = 0
 down = 0
-location = []
+location1 = []    #充电结束获取的
+loc_get1 = []
+location2 = []    #摔倒获取的
+loc_get2 = []
+location3 = []    #home获取的
+log_get3 = []
 lat_first = 0
 lon_first = 0
 lat_now = 0
 lon_now = 0
-loc_get = []
+lat_fall = 0
+lon_fall = 0
+home_lock = 0     #（home_thread调用）0:空状态    1：记录完一次经纬度
 c_lock = 0
-loc_lock = 0
+#（crutchlock，fall_det_thread调用）
+# -1：充电状态；   
+# 1：正在使用；   
+# 0：充电结束记录完初始经纬度；   
+# 2：摔倒记录完倒地经纬度
+
+
 ai = NPLUS_AI()
 ai.mode_change(1)
 tim1 = Timer(1)
@@ -194,42 +206,40 @@ def common():                                                 #平常状态(ok)
 
 #Thread
 
-def fall_det():                      
+def fall_det_thread():                      
+    global lat_first, lon_first, lat_fall, lon_fall, loc_fall, status, heartbeat_Loc
     while True:
-        global lat_first, lon_first, lat_now, lon_now, loc_lock
         common()
+        status = "ok"
+        heartbeat_Loc = None
 
         if ai.get_id_data(0) and c_lock != -1:               #识别到二维码，开始充电
             switch = 0
             c_lock = -1
-            loc_lock = 0
             
         if not ai.get_id_data(0) and c_lock == -1:         #从充电座提起断电自动记录位置——识别二维码不在就是离开出门
-            backhome = -1
             c_lock = 1
             switch = 1
             
-        if backhome == -1 and c_lock == 1:      #记录初始位置
-            if uart1.any() and loc_lock = 0:
+        if c_lock == 1:      #记录初始位置
+            if uart1.any():
                 time.sleep(0.1)
-                loc_get = uart1.readline()
-                if 'GNGLL' in loc_get:
-                    location = (str(loc_get).split(','))
-                    if location[2] == 'N':
-                        lat_first = float(location[1])
-                    elif location[2] == 'S':
-                        lat_first = float(location[1]) * -1
+                loc_get1 = uart1.readline()
+                if 'GNGLL' in loc_get1:
+                    location1 = (str(loc_get1).split(','))
+                    if location1[2] == 'N':
+                        lat_first = float(location1[1])
+                    elif location1[2] == 'S':
+                        lat_first = float(location1[1]) * -1
                     else:
                         lat_first = 0
 
-                    if location[4] == 'E':
-                        lon_first = float(location[3])
-                    elif location[4] == 'W':
-                        lon_first = float(location[3]) * -1
+                    if location1[4] == 'E':
+                        lon_first = float(location1[3])
+                    elif location1[4] == 'W':
+                        lon_first = float(location1[3]) * -1
                     else:
                         lon_first = 0
-                    
-                    loc_lock = 1
 
                     c_lock = 0             #只在充电一次结束的时候记录一次经纬度
 
@@ -261,8 +271,32 @@ def fall_det():
         
 
             if fall == 1:
-                if #导航定位
+                if uart1.any() and c_lock == 0:
+                    time.sleep(0.1)
+                    loc_get2 = uart1.readline()
+                    if 'GNGLL' in loc_get2:
+                        location2 = (str(loc_get2).split(','))
+                        if location2[2] == 'N':
+                            lat_fall = float(location2[1])
+                        elif location2[2] == 'S':
+                            lat_fall = float(location2[1]) * -1
+                        else:
+                            lat_fall = 0
+
+                        if location2[4] == 'E':
+                            lon_fall = float(location2[3])
+                        elif location2[4] == 'W':
+                            lon_fall = float(location2[3]) * -1
+                        else:
+                            lon_fall = 0
+
+                        c_lock = 2
+
+                loc_fall = {"latitude":lat_fall, 
+                            "longtitude":lon_fall}
+
                 status = "emergency"
+                heartbeat_Loc = loc_fall
                 flashlight()
                 help()
                 
@@ -273,16 +307,6 @@ def fall_det():
             elif fall == 0:
                 common()
                 music.stop()
-
-            if p5.read_digital() == 0:
-                backhome = 1
-
-            if backhome == 1 and c_lock == 0:                                                                                      #记录当前位置
-                location = list(uart1.readline())
-                latitude_now = str(float(location[19:28])) * 0.01 + str(location[29])        #存取当前纬度
-                longtitude_now = str(float(location[31:41])) * 0.01 + str(location[42])      #存取当前经度
-                #语音导航带老人回家
-                backhome = 0 #导航到家
 
         elif switch == 0:
             oled.fill(0)
@@ -295,16 +319,45 @@ def fall_det():
 
 
 def home_thread():
+    global lat_now, lon_now, home_lock, loc_get3, location3
+    while True:
+        if p5.read_digital() == 0:                #本为输出引脚，反向使用
+                backhome = 1
 
+        if backhome == 1:                                                                                      #记录当前位置
+            if uart1.any() and home_lock == 0:
+                time.sleep(0.1)
+                loc_get3 = uart1.readline()
+                if 'GNGLL' in loc_get3:
+                    location3 = (str(loc_get3).split(','))
+                    if location3[2] == 'N':
+                        lat_now = float(location3[1])
+                    elif location3[2] == 'S':
+                        lat_now = float(location3[1]) * -1
+                    else:
+                        lat_now = 0
+
+                    if location3[4] == 'E':
+                        lon_now = float(location3[3])
+                    elif location3[4] == 'W':
+                        lon_now = float(location3[3]) * -1
+                    else:
+                        lon_now = 0
+                    
+                    home_lock = 1
+            
+            #语音导航带老人回家
+            backhome = 0 #导航到家
 
 
 def heartbeat_thread():
+    global status, heartbeat_Loc
     while True:
 
         data = {
         "uuid": uuid,
         "status":status
-        "loc": None
+        "loc": heartbeat_Loc
         }
 
         time.sleep(5)
@@ -312,7 +365,7 @@ def heartbeat_thread():
         resp = urequests.post(url=BASE_URL+'/heartbeat/', data=data)
 
         if resp.code != 200:
-            print('数据传输错误')
+            print('服务器连接发生错误')
             continue
 
         resp = resp.json()
@@ -320,7 +373,9 @@ def heartbeat_thread():
         if resp['code'] == 0:
             continue
         elif resp['code'] == 1:
-            
+            print('拐杖未注册')
+            time.sleep(2)
+            continue
         else:
             print(resp['msg'])
 
@@ -335,5 +390,6 @@ except:
     print('无法连接服务器，请重试')
 else:
     _thread.start_new_thread(heartbeat_thread,())
-    _thread.start_new_thread(main_thread,())
+    _thread.start_new_thread(fall_det_thread,())
+    _thread.start_new_thread(home_thread,())
 
