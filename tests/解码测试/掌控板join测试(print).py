44a6779c8205ab754e49c447fb9e0af4b81c24a3
link857 <2761766788@qq.com>
2021-05-03 23:34:52 +0800

    another test of 'join()' and 'math'

--- tests/解码测试/掌控板join测试(print).py
+++ tests/解码测试/掌控板join测试(print).py
@@ -2,6 +2,7 @@ a = 2234.42151
 b = 11355.99864
 m = list(str(a))
 n = list(str(b))
+print(''.join(str(x) for x in [2021, '年', 21, '月', 13, '日']))
 print(m)
 print(n)
 print(''.join(m[2:]))
---
+++ tests/解码测试/解码测试.py
@@ -0,0 +1,8 @@
+import math
+
+
+a = -8.066666
+
+print(a % 15)
+print(math.ceil(a))
+print(math.floor(a))
